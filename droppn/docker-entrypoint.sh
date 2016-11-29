0385b274acc26cc05e1dc10d375336d85a849ce3
dongnan <dongyh@126.com>
2016-11-29 13:11:02 +0800

    启用supervisor

--- droppn/docker-entrypoint.sh
+++ droppn/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 supervisord -c /etc/supervisor/supervisord.conf
 
 #启动服务
-#/opt/app/gearmand/sbin/gearmand -L 127.0.0.1 -p 4730 -u root -d -l /data/logs/gearmand.log
+#/opt/app/gearmand/sbin/gearmand -P /var/run/gearmand.pid -L 127.0.0.1 -p 4730 -u root -d -l /data/logs/gearmand.log
 #/etc/init.d/mysql start
 #/etc/init.d/redis start
 #/etc/init.d/php-fpm start
