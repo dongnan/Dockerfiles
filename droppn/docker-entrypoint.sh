29c53745022c8d0d56aab60253a7b5e5e75ceeac
DongNan <dongyh@126.com>
2016-11-29 10:12:29 +0800

    update

--- droppn/docker-entrypoint.sh
+++ droppn/docker-entrypoint.sh
@@ -6,9 +6,9 @@ set -e
 
 #启动服务
 #/opt/app/gearmand/sbin/gearmand -L 127.0.0.1 -p 4730 -u root -d -l /data/logs/gearmand.log
-/etc/init.d/mysql start
-/etc/init.d/redis start
-/etc/init.d/php-fpm start
-/etc/init.d/nginx start
+#/etc/init.d/mysql start
+#/etc/init.d/redis start
+#/etc/init.d/php-fpm start
+#/etc/init.d/nginx start
 
 exec "$@"
